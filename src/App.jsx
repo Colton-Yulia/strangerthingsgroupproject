2fdd2bec2010cb573c43b47f27874b1b7de607f6
Colton Skiera <cskiera142@gmail.com>
2023-06-14 10:41:02 -0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import PostsList from "./components/PostsList";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const COHORT_NAME = "2209-FTB-ET-WEB-FT";
+  const COHORT_NAME = "2304-FTB-ET-WEB-FT";
   const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
   const [allPosts, setAllPosts] = useState([]);
 
@@ -14,7 +14,7 @@ function App() {
         const res = await fetch(`${BASE_URL}/posts`);
         const translatedData = await res.json();
         setAllPosts(translatedData.data.posts);
-        console.log(translatedData.data.postss);
+        console.log(translatedData.data.posts);
       } catch (error) {
         console.log(error);
       }
