c1b5776508427b7db265b0e2123be01977785f3d
Colton Skiera <cskiera142@gmail.com>
2023-06-17 17:20:56 -0600

    Revert "Login&Register updated"
    This reverts commit 34baeca98e51d77f649ecaa826f4f78b338715f1.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import PostsList from "./components/PostsList";
 import { Routes, Route } from "react-router-dom";
-import Login from "./components/Login";
-import Register from "./components/Login";
-import Navbar from "./components/Navbar";
 
 function App() {
   const COHORT_NAME = "2304-FTB-ET-WEB-FT";
@@ -23,15 +20,11 @@ function App() {
       }
     };
     fetchData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <>
       <Routes>
         <Route path="/" element={<PostsList allPosts={allPosts} />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/navbar" element={<Navbar />} />
       </Routes>
     </>
   );
